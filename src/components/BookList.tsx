91595acd884491cef9150c55e6e61a5f93714fb8
Henry Nguyen <nguyenhai1591@gmail.com>
2021-06-29 23:12:20 +1000

    reference

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,37 +1,106 @@
 import { values } from 'mobx'
 import { observer } from 'mobx-react'
-import { getSnapshot } from 'mobx-state-tree'
-import { useState } from 'react'
-import { Author, Book, bookAuthorStore } from '../tree/Book'
+import { getSnapshot, Instance } from 'mobx-state-tree'
+import { ChangeEvent, useState } from 'react'
+import {
+  Author,
+  Book,
+  bookAuthorStore,
+  IAuthor,
+  IBook,
+  IBookAuthorStore
+} from '../tree/Book'
 
-const HardcodedBookAndAuthor = observer(() => {
-  const john = Author.create()
-  const sherlockHolmes = Book.create({
-    id: Math.random() * 10,
-    name: 'Sherlock Holmes'
-  })
-  return (
-    <>
-      John {JSON.stringify(john, null, 4)} and Sherlock{' '}
-      {JSON.stringify(sherlockHolmes, null, 4)}
-    </>
-  )
-})
+// TS types
+interface HardcodedBookAndAuthorProps {
+  bookAuthorStore: IBookAuthorStore
+}
+
+interface AuthorPickerProps {
+  bookAuthorStore: IBookAuthorStore
+  book: IBook
+}
+
+const HardcodedBookAndAuthor = observer(
+  ({ bookAuthorStore }: HardcodedBookAndAuthorProps) => {
+    const john = Author.create({ id: '4', name: 'John' })
+    const sherlockHolmes = Book.create({
+      id: Math.random() * 10,
+      name: 'Sherlock Holmes'
+    })
+
+    // this snapshot is to check if the snapshot automatically gets updated
+    // when the store is changed, and it DOES!
+
+    // const bookStoreSnapshot = getSnapshot(bookAuthorStore)
+
+    return (
+      <>
+        John {JSON.stringify(john, null, 4)}
+        {JSON.stringify(sherlockHolmes, null, 4)}
+        {/* {JSON.stringify(bookStoreSnapshot, null, 4)} */}
+      </>
+    )
+  }
+)
+
+const AuthorPicker = observer(
+  ({ bookAuthorStore, book }: AuthorPickerProps) => {
+    const changeAuthorForBook = (event: ChangeEvent<HTMLSelectElement>) => {
+      // here we just pass in a string but as we cast it with "as", store will resolve it to a real Author
+      book.setAuthor(event.target.value as typeof book.author)
+    }
+
+    return (
+      <select value={book.author?.id} onChange={changeAuthorForBook}>
+        <option value="">-none-</option>
+        {bookAuthorStore.authors.map(author => (
+          <option value={author.id} key={author.id}>
+            {author.name}
+          </option>
+        ))}
+      </select>
+    )
+  }
+)
 
 const BookList = observer(() => {
   const [bookName, setBookName] = useState('')
+  // const bookStoreSnapshot = getSnapshot(bookAuthorStore)
 
   return (
     <div>
       HARDCODED
       <br />
-      <HardcodedBookAndAuthor />
+      <HardcodedBookAndAuthor bookAuthorStore={bookAuthorStore} />
       <br />
       <p>------------------------------------</p>
       BOOK STORE SNAPSHOT
       <br />
-      State: {JSON.stringify(getSnapshot(bookAuthorStore), null, 3)}
+      State: Authors
+      <ul>
+        {bookAuthorStore.authors.map(author => (
+          <li key={(Math.random() * 10).toString()}>{author.name}</li>
+        ))}
+      </ul>
+      State: Books
+      <ul>
+        {bookAuthorStore.books.map(book => (
+          <li key={book.id}>
+            {book.name} - {book.read.toString()}, written by {book.author?.name}
+          </li>
+        ))}
+      </ul>
       <br />
+      Read: {bookAuthorStore.completedCount} - Pending:{' '}
+      {bookAuthorStore.pendingCount}
+      <br />
+      Filtered by status 'Read':{' '}
+      <ul>
+        {bookAuthorStore.getBooksByStatus(true).map(book => (
+          <li key={book.id}>{book.name}</li>
+        ))}
+      </ul>
       <p>---------------------------------</p>
       <button
         onClick={() => {
@@ -55,6 +124,7 @@ const BookList = observer(() => {
             value={book.name}
             onChange={event => book.setName(event.target.value)}
           />
+          <AuthorPicker bookAuthorStore={bookAuthorStore} book={book} />
         </div>
       ))}
       <input
--- src/tree/Book.ts
+++ src/tree/Book.ts
@@ -1,14 +1,36 @@
-import { applySnapshot, getSnapshot, onSnapshot, types } from 'mobx-state-tree'
+import {
+  applySnapshot,
+  getSnapshot,
+  Instance,
+  onSnapshot,
+  SnapshotIn,
+  TypeOfValue,
+  types
+} from 'mobx-state-tree'
 
 // State
 const snapshots: any = []
 let currentFrame = -1
 
+const defaultAuthors = [
+  { id: '1', name: 'Conan Doyle' },
+  { id: '2', name: 'George R.R. Martin' },
+  { id: '3', name: 'Suzanne Collins' }
+]
+
+// TS declaration
+export interface IAuthor extends Instance<typeof Author> {}
+export interface IBook extends Instance<typeof Book> {}
+export interface IBookAuthorStore extends Instance<typeof RootStore> {}
+
+// Store declaration
 export const Book = types
   .model({
     id: types.number,
     name: types.optional(types.string, ''),
-    read: types.optional(types.boolean, false)
+    read: types.optional(types.boolean, false),
+    author: types.maybe(types.reference(types.late(() => Author))) // 'maybe' so author can be null, 'reference' to link to Author instance,
+    //  'late' to resolve Author after it is declared (possibly later in the file for example, like in THIS file)
   })
   .actions(self => ({
     setName: (name: string) => {
@@ -16,10 +38,23 @@ export const Book = types
     },
     toggle: () => {
       self.read = !self.read
+    },
+
+    // if you set author: IAuthor and self.author = author, then front-end must find "author" to pass into setAuthor() which is really dumb
+
+    // if you set author: any and self.author = author, then front-end can pass in anything, either "the" author or authorId only, and the store can find "the" author fine,
+    // so that in front end you can use book.author.name
+
+    // but any doesn't make sense, we would want to pass in authorId (string) from front-end, and store must be able to find "the" author based on that authorId
+    // but if you set author: string and self.author = author, and front-end pass in authorId to setAuthor(), it DOES NOT WORK!
+    // TS will show error that string (from authorId passed in) is not assignable to type self.author (which should be "an" Author instance)
+    setAuthor: (author: typeof self.author) => {
+      self.author = author
     }
   }))
 
 export const Author = types.model({
+  id: types.identifier,
   name: types.optional(types.string, '')
 })
 
@@ -36,6 +71,9 @@ const RootStore = types
     },
     get completedCount() {
       return self.books.filter(book => book.read).length
+    },
+    getBooksByStatus(status: boolean) {
+      return self.books.filter(book => book.read === status)
     }
   }))
   .actions(self => ({
@@ -44,7 +82,7 @@ const RootStore = types
       self.books.push({ id, name })
   }))
 
-export const bookAuthorStore = RootStore.create()
+export const bookAuthorStore = RootStore.create({ authors: defaultAuthors }) // you can pass in starting values here for the store
 
 // Snapshot management
 onSnapshot(bookAuthorStore, snapshot => {
@@ -80,6 +118,6 @@ bookAuthorStore.books[0].toggle()
 // })
 
 applySnapshot(bookAuthorStore, {
-  authors: [],
+  authors: defaultAuthors,
   books: [{ id: Math.random() * 10, name: 'QLD road rules', read: true }]
 })
